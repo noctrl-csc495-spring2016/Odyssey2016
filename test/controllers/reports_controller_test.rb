a6ffe21102334036eb5ca263c70b9fe5cf6958b0
Gerardo A Paleo <gerardopaleo@gmail.com>
2016-05-31 23:38:01 +0000

    Updated tests

--- test/controllers/reports_controller_test.rb
+++ test/controllers/reports_controller_test.rb
@@ -2,11 +2,25 @@ require 'test_helper'
 
 class ReportsControllerTest < ActionController::TestCase
 
-  test "should get donor report page" do
+  test "should get donors report page" do
     log_in_as(users(:bill))
     get :donor
     assert_response :success
-    assert_select "title", "Odyssey | Donor Report"
+    assert_select "title", "Odyssey | Donors"
+  end
+  
+  test "should get MapQuest report page" do
+    log_in_as(users(:bill))
+    get :mapquest
+    assert_response :success
+    assert_select "title", "Odyssey | MapQuest"
+  end
+  
+  test "should get Rejected report page" do
+    log_in_as(users(:bill))
+    get :rejected
+    assert_response :success
+    assert_select "title", "Odyssey | Rejected"
   end
   
   test "should get truck report page" do
